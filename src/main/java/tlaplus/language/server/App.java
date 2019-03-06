b9443ce797d79bedb15212a2cb50a32a2623f834
Andrew Helwer <ahelwer@microsoft.com>
2019-03-05 17:36:27 -0800

    Added Hello World java project

---
+++ src/main/java/tlaplus/language/server/App.java
@@ -0,0 +1,15 @@
+package tlaplus.language.server;
+
+import org.eclipse.lsp4j.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
